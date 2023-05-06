0213996ca9453514ff837eb46bbcd6862aba673b
Oaksas <aman.getnet2@gmail.com>
2023-05-06 23:26:53 +0300

    Add Campaign and Services section

---
+++ src/components/Landing/Campaign.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Services from "./Services";
+
+function Campaign() {
+  return (
+    <>
+      <div class="flex flex-col self-center sm:w-full md:w-1/2  gap-y-10 mx-auto  font-Lato my-40">
+        <div className="text-5xl font-black   ">
+          Your Campaign is Our Campaign
+        </div>
+        <div className="text-xl font-normal text-gray-500 ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor iure
+          blanditiis laborum. Voluptatibus esse autem dolor quia quod quas
+          consequatur quibusdam ea. Sint porro quibusdam eveniet impedit
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Campaign;
