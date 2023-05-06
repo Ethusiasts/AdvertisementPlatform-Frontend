8686b569d965a3b4b8509f4d00e593efaa59c996
Oaksas <aman.getnet2@gmail.com>
2023-05-06 13:57:30 +0300

    Add Monetize section to Landing Page

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Navigation from "../../components/Landing/Navigation";
 import Hero from "../../components/Landing/Hero";
+import "../../styles/landing.css";
+import Clients from "../../components/Landing/Clients";
+import Trust from "../../components/Landing/Trust";
+import Advertisers from "../../components/Landing/Advertisers";
+import Monetise from "../../components/Landing/Monetise";
+
 function Landing() {
   return (
     <>
       <Navigation />
       <Hero />
+      <Clients />
+      <Trust />
+      <Advertisers />
+      <Monetise />
     </>
   );
 }
