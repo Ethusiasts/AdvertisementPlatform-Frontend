6cfcda33683b5f991eb37d998e6f26b8e71131ea
Oaksas <aman.getnet2@gmail.com>
2023-05-06 14:36:22 +0300

    Add Growth section

---
+++ src/components/Landing/Growth.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Growth() {
+  return (
+    <div class="flex flex-col justify-center w-1/2 gap-y-10 mx-auto  font-Lato my-40">
+      <div className="text-5xl font-black ">Your growth is Our growth</div>
+      <div className="text-xl font-normal text-gray-500 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor iure
+        blanditiis laborum. Voluptatibus esse autem dolor quia quod quas
+        consequatur quibusdam ea. Sint porro quibusdam eveniet impedit
+      </div>
+    </div>
+  );
+}
+
+export default Growth;
