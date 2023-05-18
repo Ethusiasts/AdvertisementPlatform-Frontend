df840ae5d1f92eb192e29bba323f6cbcdf1298fd
biruk ayalew <se.biruk.ayalew@gmail.com>
2023-05-18 17:21:16 +0300

    update home page

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -1,90 +1,146 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookSquare, } from '@fortawesome/free-brands-svg-icons';
-import {  faInstagramSquare , } from '@fortawesome/free-brands-svg-icons';
-import {  faTwitterSquare , } from '@fortawesome/free-brands-svg-icons';
-import {  faRedditSquare , } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
+import { faInstagramSquare } from "@fortawesome/free-brands-svg-icons";
+import { faTwitterSquare } from "@fortawesome/free-brands-svg-icons";
+import { faRedditSquare } from "@fortawesome/free-brands-svg-icons";
 
-export default function Footer() { 
-    return (
-        <footer className="bg-gray-900 relative">
-            <div className="h-8 w-full absolute bottom-60 overflow-hidden">
-                <svg viewBox="0 0 500 150" preserveAspectRatio="none" className="h-full w-full">
-                <path d="M-1.80,81.08 C149.12,202.54 348.90,-46.45 501.02,81.08 L500.00,150.00 L0.00,150.00 Z" style={{stroke: 'none', fill: '#9caf88'}}></path>
-                </svg>
-            </div>
-            <div class="footer-line"></div>
-            <div className="container mx-auto py-8">
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 relative">
+      <div class="footer-line"></div>
+      <div className="container mx-auto py-8">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
           <div className="col-span-1 mb-4 sm:mb-0">
             <h3 className="text-lg font-semibold text-white mb-4">Solutions</h3>
             <ul className="list-none">
-              <li className="mb-2"><a href="#" className="text-gray-400 hover:text-white">Marketing</a></li>
-              <li className="mb-2"><a href="#" className="text-gray-400 hover:text-white">Analytics</a></li>
-              <li className="mb-2"><a href="#" className="text-gray-400 hover:text-white">Commerce</a></li>
-              <li className="mb-2"><a href="#" className="text-gray-400 hover:text-white">Insights</a></li>
+              <li className="mb-2">
+                <a href="#" className="text-gray-400 hover:text-white">
+                  Marketing
+                </a>
+              </li>
+              <li className="mb-2">
+                <a href="#" className="text-gray-400 hover:text-white">
+                  Analytics
+                </a>
+              </li>
+              <li className="mb-2">
+                <a href="#" className="text-gray-400 hover:text-white">
+                  Commerce
+                </a>
+              </li>
+              <li className="mb-2">
+                <a href="#" className="text-gray-400 hover:text-white">
+                  Insights
+                </a>
+              </li>
             </ul>
-        </div>
+          </div>
+
           <div className="col-span-1 mb-4 sm:mb-0">
             <h3 className="text-lg font-semibold text-white mb-4">Support</h3>
             <ul className="list-none">
-              <li className="mb-2"><a href="#" className="text-gray-400 hover:text-white">Pricing</a></li>
-              <li className="mb-2"><a href="#" className="text-gray-400 hover:text-white">Documentation</a></li>
-              <li className="mb-2"><a href="#" className="text-gray-400 hover:text-white">Guides</a></li>
-              <li className="mb-2"><a href="#" className="text-gray-400 hover:text-white">API Status</a></li>
+              <li className="mb-2">
+                <a href="#" className="text-gray-400 hover:text-white">
+                  Pricing
+                </a>
+              </li>
+              <li className="mb-2">
+                <a href="#" className="text-gray-400 hover:text-white">
+                  Documentation
+                </a>
+              </li>
+              <li className="mb-2">
+                <a href="#" className="text-gray-400 hover:text-white">
+                  Guides
+                </a>
+              </li>
+              <li className="mb-2">
+                <a href="#" className="text-gray-400 hover:text-white">
+                  API Status
+                </a>
+              </li>
             </ul>
           </div>
 
           <div className="col-span-1 mb-4 sm:mb-0">
             <h3 className="text-lg font-semibold text-white mb-4">Company</h3>
             <ul className="list-none">
-              <li className="mb-2"><a href="#" className="text-gray-400 hover:text-white">About</a></li>
-              <li className="mb-2"><a href="#" className="text-gray-400 hover:text-white">Blog</a></li>
-              <li className="mb-2"><a href="#" className="text-gray-400 hover:text-white">Partners</a></li>
-              <li className="mb-2"><a href="#" className="text-gray-400 hover:text-white">Terms</a></li>
+              <li className="mb-2">
+                <a href="#" className="text-gray-400 hover:text-white">
+                  About
+                </a>
+              </li>
+              <li className="mb-2">
+                <a href="#" className="text-gray-400 hover:text-white">
+                  Blog
+                </a>
+              </li>
+              <li className="mb-2">
+                <a href="#" className="text-gray-400 hover:text-white">
+                  Partners
+                </a>
+              </li>
+              <li className="mb-2">
+                <a href="#" className="text-gray-400 hover:text-white">
+                  Terms
+                </a>
+              </li>
             </ul>
           </div>
 
           <div className="col-span-1 mb-4 sm:mb-0">
-          <h3 className="text-lg font-semibold text-white mb-4">Let's stay connected</h3>
-          <div className="text-white">Enter your email to get notifications</div>
-          
-          <div className="flex items-center py-3 relative">
-            <input
-                className="appearance-none bg-transparent border-b-2 border-gray-400 w-full mr-3 py-1 px-2 leading-tight focus:outline-none"
+            <h3 className="text-lg font-semibold text-white mb-4">
+              Let's stay connected
+            </h3>
+            <div className="text-white">
+              Enter your email to get notifications
+            </div>
+
+            <div className="flex items-center py-3 relative">
+              <input
+                className="text-white appearance-none bg-transparent border-b-2 border-gray-400 w-full mr-3 py-1 px-2 leading-tight focus:outline-none"
                 type="text"
                 placeholder="Your Email"
-            />
-            <button
+              />
+              <button
                 className="absolute bottom-5 right-4    border-transparent text-sm text-white py-1 px-2"
                 type="button"
-            >
+              >
                 Submit
-            </button>
-        </div>
-                <div>
-                    <p className="text-white">Follow Us</p>
-                </div>    
-                    <div className='flex mt-3 text-2xl'>
-                        <a href="#" className="text-gray-400 hover:text-white">
-                            <FontAwesomeIcon icon={faFacebookSquare} className=" text-[#7D7D7D] pr-3" />
-                        </a>
-                        <a href="#" className="text-gray-400 hover:text-white">
-                            <FontAwesomeIcon icon={faInstagramSquare} className=" text-[#7D7D7D] pr-3" />
-                        </a>
-                        <a href="#" className="text-gray-400 hover:text-white">
-                            <FontAwesomeIcon icon={faTwitterSquare} className=" text-[#7D7D7D] pr-3" />
-                        </a>
-                        <a href="#" className="text-gray-400 hover:text-white">
-                            <FontAwesomeIcon icon={faRedditSquare} className=" text-[#7D7D7D] pr-3" />
-                        </a>
-                </div>
+              </button>
             </div>
+            <div>
+              <p className="text-white">Follow Us</p>
+            </div>
+            <div className="flex mt-3 text-2xl">
+              <a href="#" className="text-gray-400 hover:text-white">
+                <FontAwesomeIcon
+                  icon={faFacebookSquare}
+                  className=" text-[#7D7D7D] pr-3"
+                />
+              </a>
+              <a href="#" className="text-gray-400 hover:text-white">
+                <FontAwesomeIcon
+                  icon={faInstagramSquare}
+                  className=" text-[#7D7D7D] pr-3"
+                />
+              </a>
+              <a href="#" className="text-gray-400 hover:text-white">
+                <FontAwesomeIcon
+                  icon={faTwitterSquare}
+                  className=" text-[#7D7D7D] pr-3"
+                />
+              </a>
+              <a href="#" className="text-gray-400 hover:text-white">
+                <FontAwesomeIcon
+                  icon={faRedditSquare}
+                  className=" text-[#7D7D7D] pr-3"
+                />
+              </a>
+            </div>
+          </div>
         </div>
-        </div>
-        </footer>
-    );
+      </div>
+    </footer>
+  );
 }
-
-      
-          
-            
--- src/components/Home/Pagination.js
+++ src/components/Home/Pagination.js
@@ -1,80 +1,190 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, {
+  useState,
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+} from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import Image1 from "../../styles/assets/billboard1.jpg"
-import Image2 from "../../styles/assets/billboard2.jpg"
-import Image3 from "../../styles/assets/billboard3.jpg"
-import RecommendedCard from "./RecommendedCard";
-export default function Pagination() { 
-    const imageData = [
-        Image1, Image2, Image3, Image1
-    ]
-    const imagesPerPage = 2;
-    const totalPages = 10;
-    const imagePages = [];
-    for (let i = 0; i < totalPages; i++) {
-        const startIndex = i * imagesPerPage;
-        const endIndex = startIndex + imagesPerPage;
-        imagePages.push(imageData.slice(startIndex, endIndex));
-    }
-    
-    const [currentPage, setCurrentPage] = useState(1);
-    const handlePageChange = (pageNumber) => {
-        setCurrentPage(pageNumber);
-    }
-        
-    return (
-        <div>
-            <div className="flex flex-wrap ml-20">
-                    {imagePages[currentPage - 1].map((image) => (
-                    <RecommendedCard location="Jemo, Addis Ababa " description="Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi"
-                        imageSrc={image}
-                        alt="Card Image"
-                    />
-                    ))}
-                </div>
+import Image1 from "../../styles/assets/billboard1.jpg";
+import Image2 from "../../styles/assets/billboard2.jpg";
+import Image3 from "../../styles/assets/billboard3.jpg";
+import Card from "./BillboardCard";
+const Pagination = forwardRef((props, ref) => {
+  const [cardData, setCardData] = useState([
+    {
+      status: "Free",
+      place: "Bole",
+      city: "Addis Ababa",
+      width: 20,
+      height: 40,
+      location: "6006 John F. Kennedy Blvd, West New York, NJ 07093, USA",
+      image: Image1,
+    },
+    {
+      status: "Occupied",
+      place: "Merkato",
+      city: "Addis Ababa",
+      width: 80,
+      height: 20,
+      location: "National Stadium, Addis Ababa",
+      image: Image2,
+    },
+    {
+      status: "Free",
+      place: "Mexico",
+      city: "Addis Ababa",
+      width: 20,
+      height: 16,
+      location: "Mexico Square, Addis Ababa",
+      image: Image3,
+    },
+    {
+      status: "Occupied",
+      place: "6 kilo",
+      city: "Addis Ababa",
+      width: 10,
+      height: 20,
+      location: "6006 John F. Kennedy Blvd, West New York, NJ 07093, USA",
+      image: Image3,
+    },
+    {
+      status: "Free",
+      place: "4 kilo",
+      city: "Addis Ababa",
+      width: 90,
+      height: 10,
+      location: "Emirates stadium",
+      image: Image2,
+    },
+    {
+      status: "Free",
+      place: "Piassa",
+      city: "Addis Ababa",
+      width: 6,
+      height: 7,
+      location: "6006 John F. Kennedy Blvd, West New York, NJ 07093, USA",
+      image: Image1,
+    },
+    {
+      status: "Free",
+      place: "Megenagna",
+      city: "Addis Ababa",
+      width: 20,
+      height: 30,
+      location: "6006 John F. Kennedy Blvd, West New York, NJ 07093, USA",
+      image: Image1,
+    },
+  ]);
 
-                <div className="flex justify-center mt-4">
-                        <nav className="pagination">
-                            <ul className="flex">
-                            {currentPage > 1 && (
-                                <li className=" rounded-lg px-6 inline-block float-right shadow-md mr-6 mt-4">
-                                <button
-                                    className="px-3 py-1 rounded-md bg-white text-gray-900"
-                                            onClick={() => handlePageChange(currentPage - 1)}
-                                        >
-                                        <FontAwesomeIcon icon={faArrowLeft} className=" text-[#2785AE] pr-4" />
-                                        Previous
-                                        </button>
-                                </li>
-                            )}
-                            {Array.from({ length: totalPages }, (_, i) => (
-                                <li key={i}>
-                                <button
-                                    className={`px-3 py-1 rounded-md ${i + 1 === currentPage ? 'bg-gray-500 text-white' : 'bg-white text-gray-900'}`}
-                                        onClick={() => handlePageChange(i + 1)}
-                                >
-                                    {i + 1}
-                                </button>
-                                </li>
-                            ))}
-                            {currentPage < totalPages && (
-                                <li>
-                                <button
-                                    className="px-3 py-1 rounded-md bg-white text-gray-900"
-                                            onClick={() => handlePageChange(currentPage + 1)}
-                                >
-                                            Next
-                            <FontAwesomeIcon icon={faArrowRight} className=" text-[#2785AE] text-base pl-4" />
-                                            
-                                </button>
-                                </li>
-                            )}
-                            </ul>
-                        </nav>
-                </div>
-        </div>
-        );
-    
-}
+  const imagesPerPage = 6;
+  const totalPages = 10;
+  const cardPages = [];
+  for (let i = 0; i < totalPages; i++) {
+    const startIndex = i * imagesPerPage;
+    const endIndex = startIndex + imagesPerPage;
+    cardPages.push(cardData.slice(startIndex, endIndex));
+  }
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  useImperativeHandle(ref, () => ({
+    handleSort(property) {
+      const sortedData = [...cardData].sort((a, b) => {
+        if (property === "size") {
+          let a_val = a["width"] * a["height"];
+          let b_val = b["width"] * b["height"];
+          if (a_val < b_val) {
+            return -1;
+          }
+          if (a_val > b_val) {
+            return 1;
+          }
+          return 0;
+        }
+        if (a[property] < b[property]) {
+          return -1;
+        }
+        if (a[property] > b[property]) {
+          return 1;
+        }
+        return 0;
+      });
+      setCardData(sortedData);
+    },
+  }));
+
+  return (
+    <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 pl-11 ">
+        {cardPages[currentPage - 1].map((card) => (
+          <Card
+            status={card.status}
+            place={card.place}
+            city={card.city}
+            width={card.width}
+            height={card.height}
+            location={card.location}
+            imageSrc={card.image}
+            alt="Card Image"
+          />
+        ))}
+      </div>
+
+      <div className="flex justify-center mt-4">
+        <nav className="pagination">
+          <ul className="flex">
+            {currentPage > 1 && (
+              <li className=" rounded-lg px-6 inline-block float-right shadow-md mr-6">
+                <button
+                  className="px-3 py-1 rounded-md bg-white text-gray-900"
+                  onClick={() => handlePageChange(currentPage - 1)}
+                >
+                  <FontAwesomeIcon
+                    icon={faArrowLeft}
+                    className=" text-[#2785AE] pr-4"
+                  />
+                  Previous
+                </button>
+              </li>
+            )}
+            {Array.from({ length: totalPages }, (_, i) => (
+              <li key={i}>
+                <button
+                  className={`px-3 py-1 rounded-md ${
+                    i + 1 === currentPage
+                      ? "bg-gray-500 text-white"
+                      : "bg-white text-gray-900"
+                  }`}
+                  onClick={() => handlePageChange(i + 1)}
+                >
+                  {i + 1}
+                </button>
+              </li>
+            ))}
+            {currentPage < totalPages && (
+              <li className="rounded-lg px-6 inline-block shadow-md">
+                <button
+                  className="px-3 py-1 rounded-md bg-white text-gray-900"
+                  onClick={() => handlePageChange(currentPage + 1)}
+                >
+                  Next
+                  <FontAwesomeIcon
+                    icon={faArrowRight}
+                    className=" text-[#2785AE] text-base pl-4"
+                  />
+                </button>
+              </li>
+            )}
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+});
+
+export default Pagination;
