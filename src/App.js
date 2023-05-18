79a97b3e8d9cb64b7ee67a5ddfed0483f04853d5
Abenezer-Fekadu <se.abenezer.fekadu@gmail.com>
2023-05-18 20:49:11 +0300

    app update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import Home from "./pages/Home";
-import Landing from "./pages/Landing/Landing";
+// import Landing from "./pages/Landing/Landing";
 import SignIn from "./pages/auth/signIn";
 import Detail from "./pages/detail_page/Detail";
-import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
   },
+  // {
+  //   path: "/landing",
+  //   element: <Landing />,
+  // },
   {
-    path: "/landing",
-    element: <Landing />,
+    path: "/details",
+    element: <Detail />,
+  },
+  {
+    path: "/signIn",
+    element: <SignIn />,
   },
 ]);
 export default function App() {
