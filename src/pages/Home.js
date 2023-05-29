c89440771719beffbcab51f6ed485de59a581080
Oaksas <aman.getnet2@gmail.com>
2023-05-29 22:41:17 +0300

    Edit home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,13 +4,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSort } from "@fortawesome/free-solid-svg-icons";
 import { faThList } from "@fortawesome/free-solid-svg-icons";
 import { faList } from "@fortawesome/free-solid-svg-icons";
-import Image1 from "../styles/assets/billboard1.jpg";
-import Image2 from "../styles/assets/billboard2.jpg";
 import Pagination from "../components/Home/Pagination";
 import Footer from "../components/Home/Footer";
 import { faCaretUp } from "@fortawesome/free-solid-svg-icons";
-import RecommendedCard from "../components/Home/RecommendedCard";
-import HomeNavbar from "../components/Home/HomeNavbar";
 import BillboardFilterBar from "../components/Home/BillboardFilterBar";
 import SearchBox from "../components/Home/SearchBox";
 import PopUp from "../components/Home/PopUp";
@@ -19,9 +15,8 @@ import classNames from "classnames";
 import { Link } from "react-router-dom";
 import Help from "../components/Home/Help";
 import Hero from "../components/Home/Hero";
-import Navigation from "../components/Landing/Navigation";
 import RecommendedCarousel from "../components/Home/RecommendedCarousel";
-import { Carousel } from "flowbite-react";
+
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
   const [sortBy, setSortBy] = useState(null);
