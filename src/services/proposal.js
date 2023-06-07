8a6cd56efd37f55b74054a94b3fb124d9feb3b67
Abenezer-Fekadu <se.abenezer.fekadu@gmail.com>
2023-06-07 23:08:07 +0300

    fixed cart detail page

--- src/services/proposal.js
+++ src/services/proposal.js
@@ -1,4 +1,4 @@
-import axiosInstance from "../utils/axiosInstance";
+import { axiosInstance } from "../utils/axiosInstance";
 export const createProposal = (proposal) => {
   console.log(proposal);
   return axiosInstance
